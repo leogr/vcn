46ef241df7bfa0f4696d94713101bf81b1c67d64
Leonardo Grasso <me@leonardograsso.com>
2019-07-01 11:58:51 +0200

    feat(serve): `/verify` endpoint

--- pkg/cmd/serve/verify.go
+++ pkg/cmd/serve/verify.go
@@ -10,6 +10,7 @@ package serve
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/vchain-us/vcn/pkg/api"
@@ -21,9 +22,51 @@ func verify(w http.ResponseWriter, r *http.Request) {
 	hash := vars["hash"]
 	user, _ := currentUser()
 
-	verification, err := api.BlockChainVerify(hash)
+	var keys []string
+	org := r.URL.Query().Get("org")
+	if org != "" {
+		bo, err := api.BlockChainGetOrganisation(org)
+		if err != nil {
+			writeErrorResponse(w, "organization error", err, http.StatusBadRequest)
+			return
+		}
+		keys = bo.MembersKeys()
+	} else {
+		ks := r.URL.Query().Get("keys")
+		if ks != "" {
+			keys = strings.Split(ks, ",")
+			// add 0x if missing, lower case
+			for i, k := range keys {
+				if !strings.HasPrefix(k, "0x") {
+					keys[i] = "0x" + k
+				}
+				keys[i] = strings.ToLower(keys[i])
+			}
+		}
+	}
+
+	var verification *api.BlockchainVerification
+	var err error
+	// if keys have been passed, check for a verification matching them
+	if len(keys) > 0 {
+		verification, err = api.BlockChainVerifyMatchingPublicKeys(hash, keys)
+	} else {
+		// if we have an user, check for verification matching user's keys first
+		if hasAuth, _ := user.IsAuthenticated(); hasAuth {
+			if userKeys := user.Keys(); len(userKeys) > 0 {
+				verification, err = api.BlockChainVerifyMatchingPublicKeys(hash, userKeys)
+			}
+		}
+		// if no user nor verification matching the user has found,
+		// fallback to the last with highest level available verification
+		if verification.Unknown() {
+			verification, err = api.BlockChainVerify(hash)
+		}
+	}
+
 	if err != nil {
-		panic(err)
+		writeErrorResponse(w, "verification error", err, http.StatusBadRequest)
+		return
 	}
 
 	var artifact *api.ArtifactResponse
